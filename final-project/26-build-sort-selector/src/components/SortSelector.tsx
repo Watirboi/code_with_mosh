0ad4cfc1bcc67e8095ca6b2383692a6c57f763ec
KP Whitfield <kwhit@hotmail.com>
2024-05-29 19:13:28 -0500

    Build sort selector

---
+++ final-project/26-build-sort-selector/src/components/SortSelector.tsx
@@ -0,0 +1,26 @@
+import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
+import { BsChevronDown } from 'react-icons/bs';
+
+const SortSelector = () => {
+    return (
+        <Menu>
+          <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+            {/* {selectedPlatform?.name || 'Platforms'} */}
+            Order by: Relevance
+          </MenuButton>
+          <MenuList>
+            {/* {data.map((platform) => (
+              <MenuItem onClick={() => onSelectPlatform(platform)} key={platform.id}>{platform.name}</MenuItem>
+            ))} */}
+            <MenuItem>Relevance</MenuItem>
+            <MenuItem>Date Added</MenuItem>
+            <MenuItem>Name</MenuItem>
+            <MenuItem>Release Date</MenuItem>
+            <MenuItem>Popularity</MenuItem>
+            <MenuItem>Average Rating</MenuItem>
+          </MenuList>
+        </Menu>
+      );
+}
+
+export default SortSelector
